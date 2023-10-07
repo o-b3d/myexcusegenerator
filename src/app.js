73661a5d06fe51359ee6e883aa73bdd70303b32d
Obed Gonzalez <130100856+o-b3d@users.noreply.github.com>
2023-10-07 19:02:30 -0400

    initial commit

--- src/app.js
+++ src/app.js
@@ -7,5 +7,36 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  let who = ["The spaceship", "My kite", "His bear", "My laptop"];
+
+  let action = ["ran over", "flew over", "hit", "exploded"];
+
+  let what = ["my skateboard", "the roller coaster", "the shed"];
+
+  let when = [
+    "before my Uncle's funeral",
+    "exactly on time",
+    "when I got home",
+    "during my break",
+    "while I ran"
+  ];
+
+  function randomElement(array) {
+    return array[Math.floor(Math.random() * array.length)];
+  }
+
+  let myexcuse =
+    randomElement(who) +
+    " " +
+    randomElement(action) +
+    " " +
+    randomElement(what) +
+    " " +
+    randomElement(when);
+
+  console.log(myexcuse);
+
+  const excuseElement = document.getElementsByTagName("p");
+
+  excuseElement[0].innerText = myexcuse;
 };
