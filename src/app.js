71773c49452dbeeb3bd573c56074553b1ebe32ec
Obed Gonzalez <130100856+o-b3d@users.noreply.github.com>
2023-10-08 20:03:28 -0400

    updated app.js with string concat

--- src/app.js
+++ src/app.js
@@ -7,36 +7,5 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  let who = ["The spaceship", "My kite", "His bear", "My laptop"];
-
-  let action = ["ran over", "flew over", "hit", "exploded"];
-
-  let what = ["my skateboard", "the roller coaster", "the shed"];
-
-  let when = [
-    "before my Uncle's funeral",
-    "exactly on time",
-    "when I got home",
-    "during my break",
-    "while I ran"
-  ];
-
-  function randomElement(array) {
-    return array[Math.floor(Math.random() * array.length)];
-  }
-
-  let myexcuse =
-    randomElement(who) +
-    " " +
-    randomElement(action) +
-    " " +
-    randomElement(what) +
-    " " +
-    randomElement(when);
-
-  console.log(myexcuse);
-
-  const excuseElement = document.getElementsByTagName("p");
-
-  excuseElement[0].innerText = myexcuse;
+  console.log("Hello Rigo from the console!");
 };
