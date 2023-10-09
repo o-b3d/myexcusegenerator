126fe9ffce9f2d032782c2d8acd190173df36c31
Obed Gonzalez <130100856+o-b3d@users.noreply.github.com>
2023-10-08 20:04:26 -0400

    correctly uploaded correct app.js

--- src/app.js
+++ src/app.js
@@ -7,5 +7,33 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+
+  let who = ["The spaceship", "My kite", "His bear", "My laptop"];
+
+  let action = ["ran over", "flew over", "hit", "exploded"];
+
+  let what = ["my skateboard", "the roller coaster", "the shed"];
+
+  let when = [
+    "before my Uncle's funeral",
+    "exactly on time",
+    "when I got home",
+    "during my break",
+    "while I ran"
+  ];
+
+  function getRandomWord(array) {
+    return array[Math.floor(Math.random() * array.length)];
+  }
+
+  const randomWho = getRandomWord(who);
+  const randomAction = getRandomWord(action);
+  const randomWhat = getRandomWord(what);
+  const randomWhen = getRandomWord(when);
+
+  const myexcuse = `${randomWho} ${randomAction} ${randomWhat} ${randomWhen}`;
+
+  const excuseElement = document.getElementsByTagName("p");
+
+  excuseElement[0].innerText = myexcuse;
 };
